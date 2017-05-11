f1b5c437e246e4d8a7e852ed476fee98d9186ded
Aaron Ellis <aaronbrentellis@gmail.com>
2017-05-11 16:36:58 -0600

    added an api call

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
 export default class SearchBar extends Component {
     constructor(props) {
@@ -14,9 +17,15 @@ export default class SearchBar extends Component {
         this.setState ({ term: event.target.value });
     }
 
+    onFormSubmit(event){
+        event.preventDefault();
+
+        // We need to go and fetch weather data
+    }
+
     render(){
         return (
-            <form className="input-group">
+            <form onSubmit={this.onFormSubmit} className="input-group">
                 <input
                     placeholder="Get a five-day forecast in your favorite cities"
                     className="form-control"
