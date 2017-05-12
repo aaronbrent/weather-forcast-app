ab3167159f3996e0adfd45382e709613ce0ef995
Aaron Ellis <aaronbrentellis@gmail.com>
2017-05-11 18:34:33 -0600

    added redux-promise

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,14 @@
 import axios from 'axios'
 
-const API_KEY = 'b1b15e88fa797225412429c1c50c122a1';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const API_KEY = 'f3f444b5f2d0626d65c68c8e48d59127';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
     const request = axios.get(url);
+
 
 
     return {
